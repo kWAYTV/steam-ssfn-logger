476345a9a2757227c50ef838a0694baabbef534a
kWAY <55437151+kWAYTV@users.noreply.github.com>
2023-08-19 04:52:05 +0200

    Update ssfn.py

--- src/handler/ssfn.py
+++ src/handler/ssfn.py
@@ -7,10 +7,12 @@ class SSFNHandler:
         }
 
     def download(self, ssfn, steam_path):
+        removed_files = 0
         url = f'https://ssfnbox.com/download/ssfn{ssfn}'
         print("Requesting SSFN file...")
 
         response = requests.get(url, headers=self.headers)
+        response.raise_for_status()
         body = response.text
 
         sec_value = re.search(r'window\.location\.assign\("/download/ssfn\d+\?sec=(\w+?)"\)', body)
@@ -25,7 +27,10 @@ class SSFNHandler:
         for file in os.listdir(steam_path):
             if file.startswith(f'ssfn'):
                 os.remove(os.path.join(steam_path, file))
-                print("Removed old SSFN file")
+                removed_files += 1
+        
+        if removed_files > 0:
+            print(f"Removed {removed_files} old ssfn files.")
 
         filename = os.path.join(steam_path, f'ssfn{ssfn}')
         with open(filename, 'wb') as f:
